36181ab987475eed0a00a44989de979583b5ddd6
rachid-buzurtanov <146711214+rachid-buzurtanov@users.noreply.github.com>
2024-06-17 16:39:39 +0200

    Add files via upload

---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    /*
+    'public' means any object can access it.
+
+    'static' means that this method is associated with the class, and not
+    a specific instance of that class
+
+    'void' means it returns nothing
+    */
+
+    // 'String[] args' -> CLI argument
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
